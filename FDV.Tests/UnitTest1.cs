0f3aa71506db5dbeb29a88c484dc31609ced02e9
prestamo_admin <prestamo_admin@etsiinf24>
2025-10-31 14:11:33 +0100

    Carpeta para los Tests

---
+++ FDV.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FDV.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
